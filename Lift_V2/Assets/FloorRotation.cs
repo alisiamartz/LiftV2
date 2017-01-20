b827504f81f6fd7dc45fdccbcdc40acfe5e563ec
bion719 <djchambe@ucsc.edu>
2017-01-19 18:16:44 -0800

    Elevator Dial and Manager

---
+++ Lift_V2/Assets/FloorRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorRotation : MonoBehaviour {
+
+    GameObject Manager;
+    private float dialFloorPos;
+
+	// Use this for initialization
+	void Start () {
+		Manager = GameObject.FindGameObjectWithTag("ElevatorManager");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //If Floor has been changed and dial not updated
+        if(dialFloorPos != Manager.GetComponent<Manager>().floorPos)
+        {
+            //90 is floor 0 and -90 is max floor (4)
+            transform.rotation = Quaternion.Euler(0, 0, 90 - Manager.GetComponent<Manager>().floorPos * 36);
+        }
+
+	}
+}
