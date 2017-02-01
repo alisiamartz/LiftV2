e5f12e58928fdcb0d0fdaddff1f58e0da0382ad6
demois <rjuan3595@yahoo.com>
2017-02-01 11:07:28 -0800

    clock face script

---
+++ Lift_V2/Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClockAnimator : MonoBehaviour {
+
+    private const float
+        hoursToDegrees = 360f / 12f,
+        minutesToDegrees = 360f / 60f;
+
+    public Transform hours, minutes;
+
+    void Update() {
+        float time = Time.timeSinceLevelLoad;
+        float minute = time % 60f;
+        float hour = time / 60f;
+
+        hours.localRotation =
+            Quaternion.Euler(0f, 0f, hour * -hoursToDegrees + 4* hoursToDegrees);
+        minutes.localRotation =
+            Quaternion.Euler(0f, 0f, minute * -minutesToDegrees);
+    }
+}
