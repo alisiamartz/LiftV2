bb72943e42d9f51e5c01cc20c711e501cb5b4f72
derrekklauer <dklauer@ucsc.edu>
2017-02-08 13:56:01 -0800

    Sound Manager and Sliding Door
    Added the sliding door which can be tested by pressing l (open) and k
    (close). the test code is in the update function of slidingDoor2, and
    should be removed after alissia pulls the trigger from it (or whenever
    we are done testing it).
    ALSO, I added a basic implementation of a sound test (press j to hear a
    localized sound). I included basic instructions for setting up
    additional sounds within the slidingDoor2 script as well:
    /*example of playing a sound. doesnt work well with update calling the
    sound multiple times (and im just trying to show proof of concept)
    * in the code where it will actually be used, it should only called once
    per sound. import sound files into the AudioClips file
    * to add additional sounds, copy an existing prefab within SoundGroups
    and modify the elements by including the sound pool you want to play
    * pitch is optional. make sure spatial blend is set to 3d. sound prefabs
    must remain in SoundGroups folder to be located by the script
    * create a variable for the object creating the sound <doorSFX> and in
    the inspector set that field to the same name as the prefab you want
    from SoundGroups
    * some additional sounds are in the googledrive*/
    
    additionally, there is a sound test scene that came with the aduio
    manager if you want to do any further testing.

---
+++ Lift_V2/Assets/Scripts/slidingDoor2.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+//animation is currently scaled based on the SIZE (not positioning) of the door, if we choose to re-size the models, the animation may need to be modified
+//the door will most likely need to be thinner/moved in order to not clip with the lever wall, this should not interfere with the animation
+//*we also need a trigger that prevents the player from moving the elevator while the door is open
+public class slidingDoor2 : MonoBehaviour
+{
+
+    Animator slidingDoor;
+    bool doorOpen;
+    public string doorSFX;
+    void Start()
+    {
+        doorOpen = false;
+        slidingDoor = GetComponent<Animator>();
+    }
+
+    public void openSlidingDoor()
+    {
+        if (!doorOpen)
+        {
+            doorOpen = true;
+            DoorControl("open");
+        }
+    }
+
+    public void closeSlidingDoor()
+    {
+        if (doorOpen)
+        {
+            doorOpen = false;
+            DoorControl("closed");
+        }
+    }
+
+    void DoorControl(string direction)
+    {
+        slidingDoor.SetTrigger(direction);
+    }
+
+    void Update()
+    {//----------------------------------------Only used for testing purposes, will be removed
+        if (Input.GetKey("l"))//open
+            openSlidingDoor();
+        if (Input.GetKey("k"))//close
+            closeSlidingDoor();
+
+
+        /*example of playing a sound. doesnt work well with update calling the sound multiple times (and im just trying to show proof of concept) 
+         * in the code where it will actually be used, it should only called once per sound. import sound files into the AudioClips file
+         * to add additional sounds, copy an existing prefab within SoundGroups and modify the elements by including the sound pool you want to play
+         * pitch is optional. make sure spatial blend is set to 3d. sound prefabs must remain in SoundGroups folder to be located by the script
+         * create a variable for the object creating the sound <doorSFX> and in the inspector set that field to the same name as the prefab you want from SoundGroups
+         * some additional sounds are in the googledrive*/
+
+        if (Input.GetKey("j"))
+            doorSFX.PlaySound(transform.position);
+    }//----------------------------------------
+
+}
