1e6fd71fc108caa975e82c33245bc0315feb7900
demois <rjuan3595@yahoo.com>
2017-02-17 14:27:11 -0800

    Added prototype interactables

--- Lift_V2/Assets/Scripts/InteractiveObjects.cs
+++ Lift_V2/Assets/Scripts/InteractiveObjects.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class InteractiveObjects : MonoBehaviour {
 
+    private Rigidbody rb;
+    public GameObject item1;
+    public GameObject item2;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("objA")) {
+            other.gameObject.SetActive(false);
+        }
+        if (other.gameObject.CompareTag("objB")) {
+            other.gameObject.SetActive(false);
+        }
+    }
+        // Update is called once per frame
+        void Update () {
 		
 	}
 }
