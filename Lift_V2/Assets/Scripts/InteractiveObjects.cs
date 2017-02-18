a4181c9638e450c45e41cb2267b55f943a7ec480
demois <rjuan3595@yahoo.com>
2017-02-17 17:23:57 -0800

    Improved Interactables script

--- Lift_V2/Assets/Scripts/InteractiveObjects.cs
+++ Lift_V2/Assets/Scripts/InteractiveObjects.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class InteractiveObjects : MonoBehaviour {
 
+    //When the player holds either hand controller on the object and holds trigger, an interaction will occur 
     private Rigidbody rb;
     public GameObject item1;
     public GameObject item2;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
+    [SerializeField]
+    SteamVR_TrackedObject trackedObj;
+    [SerializeField]
+    SteamVR_TrackedObject trackedObj2;
+
+
+    private SteamVR_Controller.Device device {
+        get {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
     }
 
-    void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("objA")) {
-            other.gameObject.SetActive(false);
-        }
-        if (other.gameObject.CompareTag("objB")) {
-            other.gameObject.SetActive(false);
+    private SteamVR_Controller.Device device2 {
+        get {
+            return SteamVR_Controller.Input((int)trackedObj2.index);
         }
     }
-        // Update is called once per frame
-        void Update () {
-		
-	}
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        item1 = GameObject.FindGameObjectWithTag("objA");
+
+        item2 = GameObject.FindGameObjectWithTag("objB");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        //if left/right hand on cube object and trigger is pressed, makes cube disappear
+        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj.transform.position, item1.transform.position) < .15) || 
+            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj2.transform.position, item1.transform.position) < .15)) {
+            item1.SetActive(false);
+        }
+        //if left/right hand on sphere object and trigger is pressed, makes sphere disappear
+        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj.transform.position, item2.transform.position) < .15) || 
+            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj2.transform.position, item2.transform.position) < .15)) {
+            item2.SetActive(false);
+        }
+    }
 }
