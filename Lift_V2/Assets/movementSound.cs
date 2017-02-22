b1fcb782ac4bd08e7fff186631ef8f227254bc09
bion719 <djchambe@ucsc.edu>
2017-02-21 20:00:48 -0800

    Elevator Movement sounds should work. Script disabled until tested

--- Lift_V2/Assets/movementSound.cs
+++ Lift_V2/Assets/movementSound.cs
@@ -6,19 +6,26 @@ public class movementSound : MonoBehaviour {
 
     public float timeToEngine = 5.0f;
     private bool revvingUp = false;
+    private bool elevatorStopped = true;
     private float timer = 0.0f;
 
     public string elevatorStartSound;
     public string elevatorEngineSound;
 
-	// Use this for initialization
-	void Start () {
-        elevatorStartSound.PlaySound();
-        revvingUp = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
+        if(GetComponent<ElevatorMovement>().liftSpeedCurrent != 0 && elevatorStopped == true) {
+            elevatorStartSound.PlaySound();
+            revvingUp = true;
+            elevatorStopped = false;
+        }
+        else if(GetComponent<ElevatorMovement>().liftSpeedCurrent == 0 && elevatorStopped == false) {
+            //Stop playing the elevator engine sound
+            GameObject myObject = GameObject.Find("ElevatorMovement");
+            myObject.GetComponent<SoundGroup>().pingSound();
+            elevatorStopped = true;
+        }
+
 		if(revvingUp == true) {
             if(timer < timeToEngine) {
                 timer += 0.1f;
