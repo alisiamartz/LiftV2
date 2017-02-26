73b52196a9493560cea1b308edd00a9bd8ba7fb8
demois <rjuan3595@yahoo.com>
2017-02-25 19:34:06 -0800

    interactive objects update

--- Lift_V2/Assets/Scripts/InteractiveObjects.cs
+++ Lift_V2/Assets/Scripts/InteractiveObjects.cs
@@ -13,8 +13,9 @@ public class InteractiveObjects : MonoBehaviour {
     SteamVR_TrackedObject trackedObj;
     [SerializeField]
     SteamVR_TrackedObject trackedObj2;
-
-
+    private GameObject item1A;
+    private string thisHand;
+    private bool released;
     private SteamVR_Controller.Device device {
         get {
             return SteamVR_Controller.Input((int)trackedObj.index);
@@ -32,22 +33,61 @@ public class InteractiveObjects : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
 
         item1 = GameObject.FindGameObjectWithTag("objA");
-
+        item1A = GameObject.FindGameObjectWithTag("objA");
         item2 = GameObject.FindGameObjectWithTag("objB");
+        released = false;
+    }
+
+    //will give it throw physics
+    void Launch(string hand) {
+
     }
 
 
     // Update is called once per frame
     void Update () {
         //if left/right hand on cube object and trigger is pressed, makes cube disappear
-        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj.transform.position, item1.transform.position) < .15) || 
-            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj2.transform.position, item1.transform.position) < .15)) {
-            item1.SetActive(false);
+        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj.transform.position, item1.transform.position) < .2) || 
+            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj2.transform.position, item1.transform.position) < .2)) {
+            item1.GetComponent<Rigidbody>().isKinematic = true;
+            item1.transform.rotation = Quaternion.identity;
+            if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+            {
+            item1.transform.position = trackedObj.transform.position;
+            }
+            else
+            {
+            item1.transform.position = trackedObj2.transform.position;
+            }
+        }
+        else {
+            item1.GetComponent<Rigidbody>().isKinematic = false;
+            item1.transform.rotation = Quaternion.identity;
         }
         //if left/right hand on sphere object and trigger is pressed, makes sphere disappear
-        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj.transform.position, item2.transform.position) < .15) || 
-            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj2.transform.position, item2.transform.position) < .15)) {
-            item2.SetActive(false);
+        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj.transform.position, item2.transform.position) < .2) ||
+            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj2.transform.position, item2.transform.position) < .2))
+        {
+            item2.GetComponent<Rigidbody>().isKinematic = true;
+            item2.transform.rotation = Quaternion.identity;
+            if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+            {
+                item2.transform.position = trackedObj.transform.position;
+                released = true;
+                thisHand = "right";
+            }
+            else
+            {
+                item2.transform.position = trackedObj2.transform.position;
+                released = true;
+                thisHand = "left";
+            }
+        }
+        else
+        {
+            item2.GetComponent<Rigidbody>().isKinematic = false;
+            item2.transform.rotation = Quaternion.identity;
+            if (released) { Launch(thisHand); }
         }
     }
 }
