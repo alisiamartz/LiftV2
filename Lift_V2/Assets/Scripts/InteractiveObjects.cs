c53048ef9ee8d993f52f43d42cf652f67c49b2cc
demois <rjuan3595@yahoo.com>
2017-02-27 17:33:35 -0800

    fixed object physics & bugs

--- Lift_V2/Assets/Scripts/InteractiveObjects.cs
+++ Lift_V2/Assets/Scripts/InteractiveObjects.cs
@@ -14,8 +14,15 @@ public class InteractiveObjects : MonoBehaviour {
     [SerializeField]
     SteamVR_TrackedObject trackedObj2;
     private GameObject item1A;
-    private string thisHand;
-    private bool released;
+    private GameObject item2A;
+    private float item1T;
+    private float item2T;
+    float x1;
+    float y1;
+    float z1;
+    float x2;
+    float y2;
+    float z2;
     private SteamVR_Controller.Device device {
         get {
             return SteamVR_Controller.Input((int)trackedObj.index);
@@ -33,61 +40,66 @@ public class InteractiveObjects : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
 
         item1 = GameObject.FindGameObjectWithTag("objA");
-        item1A = GameObject.FindGameObjectWithTag("objA");
+        x1 = item1.transform.position.x;
+        y1 = item1.transform.position.y;
+        z1 = item1.transform.position.z;
         item2 = GameObject.FindGameObjectWithTag("objB");
-        released = false;
+        x2 = item2.transform.position.x;
+        y2 = item2.transform.position.y;
+        z2 = item2.transform.position.z;
+        item1T = 0.0f;
+        item2T = 0.0f;
     }
-
-    //will give it throw physics
-    void Launch(string hand) {
-
-    }
-
 
     // Update is called once per frame
     void Update () {
         //if left/right hand on cube object and trigger is pressed, makes cube disappear
-        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj.transform.position, item1.transform.position) < .2) || 
-            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj2.transform.position, item1.transform.position) < .2)) {
+        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj.transform.position, item1.transform.position) < .2) || 
+            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj2.transform.position, item1.transform.position) < .2)) {
             item1.GetComponent<Rigidbody>().isKinematic = true;
             item1.transform.rotation = Quaternion.identity;
-            if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+            if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
             {
-            item1.transform.position = trackedObj.transform.position;
+                item1.transform.position = trackedObj.transform.position;
+                item1T = 5.0f;
             }
             else
             {
-            item1.transform.position = trackedObj2.transform.position;
+                item1.transform.position = trackedObj2.transform.position;
+                item1T = 5.0f;
             }
         }
         else {
+            //after 5 seconds, cube returns back on the shelf
             item1.GetComponent<Rigidbody>().isKinematic = false;
             item1.transform.rotation = Quaternion.identity;
+            item1T -= Time.deltaTime;
+            if (item1T <= 0) { item1.transform.position = new Vector3(x1,y1,z1); }
         }
         //if left/right hand on sphere object and trigger is pressed, makes sphere disappear
-        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj.transform.position, item2.transform.position) < .2) ||
-            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad) && Vector3.Distance(trackedObj2.transform.position, item2.transform.position) < .2))
+        if ((device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj.transform.position, item2.transform.position) < .2) ||
+            (device2.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger) && Vector3.Distance(trackedObj2.transform.position, item2.transform.position) < .2))
         {
             item2.GetComponent<Rigidbody>().isKinematic = true;
             item2.transform.rotation = Quaternion.identity;
-            if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad))
+            if (device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
             {
                 item2.transform.position = trackedObj.transform.position;
-                released = true;
-                thisHand = "right";
+                item2T = 5.0f;
             }
             else
             {
                 item2.transform.position = trackedObj2.transform.position;
-                released = true;
-                thisHand = "left";
+                item2T = 5.0f;
             }
         }
         else
         {
+            //after 5 seconds, sphere returns back on the shelf
             item2.GetComponent<Rigidbody>().isKinematic = false;
             item2.transform.rotation = Quaternion.identity;
-            if (released) { Launch(thisHand); }
+            item2T -= Time.deltaTime;
+            if (item2T <= 0) { item2.transform.position = new Vector3(x2, y2, z2); }
         }
     }
 }
