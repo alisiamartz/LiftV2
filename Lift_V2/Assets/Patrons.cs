78986d6d5707e8fea4f92366f9397f051d6e9777
bion719 <djchambe@ucsc.edu>
2017-04-23 12:00:01 -0700

    New Day Manager
    Adjust patron specifics in Patrons script. Timeline is in Day Manager

---
+++ Lift_V2/Assets/Patrons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrons : MonoBehaviour {
+
+    public class Patron {
+        public GameObject prefab;
+        public int startFloor;
+
+        public Patron(GameObject preF, int sFloor) {
+            prefab = preF;
+            startFloor = sFloor;
+        }
+    }
+
+    public Patron fetchPatron(string patronName) {
+
+        if(patronName == "Boss1") {
+            var startFloor = 1;
+            var prefab = (GameObject)Resources.Load("/Patrons/yourPrefab");
+            return new Patron(prefab, startFloor);
+        }
+
+        Debug.LogError(patronName + " is not a valid patronName. Format with the patron name and the conversation number: 'Boss1'");
+        return null;
+    }
+}
