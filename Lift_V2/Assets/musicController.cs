7e22771433c8bc2f52986694c2eebacadecfe79f
bion719 <djchambe@ucsc.edu>
2017-05-01 02:01:18 -0700

    Added music files and controller

---
+++ Lift_V2/Assets/musicController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musicController : MonoBehaviour {
+
+    public AudioClip elevatorMusic;
+    public AudioClip mainTheme;
+    public AudioClip bossTheme;
+    public AudioClip businessTheme;
+    public AudioClip touristTheme;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
