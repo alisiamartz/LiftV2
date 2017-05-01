8f88130375c57b6ae2543e801b1293395498e4fe
bion719 <djchambe@ucsc.edu>
2017-04-30 22:47:45 -0700

    lightsFixed

--- Lift_V2/Assets/patronWaiting.cs
+++ Lift_V2/Assets/patronWaiting.cs
@@ -4,21 +4,7 @@ using UnityEngine;
 
 public class patronWaiting : MonoBehaviour {
 
-    public GameObject FloorBLight;
-    public GameObject FloorLLight;
-    public GameObject Floor2Light;
-    public GameObject Floor3Light;
-    public GameObject Floor4Light;
-    public GameObject Floor5Light;
-    public GameObject Floor6Light;
-
-    private GameObject[] FloorLights;
-
-    // Use this for initialization
-    void Start () {
-        //Fill the Array with the parents for the lights
-        FloorLights[0] = FloorBLight; FloorLights[1] = FloorLLight; FloorLights[2] = Floor2Light; FloorLights[3] = Floor3Light; FloorLights[4] = Floor4Light; FloorLights[5] = Floor5Light; FloorLights[6] = Floor6Light;
-    }
+    public GameObject[] FloorLights;
 
     public void lightOn(int floor) {
         FloorLights[floor].transform.Find("lightOn").gameObject.SetActive(true);
