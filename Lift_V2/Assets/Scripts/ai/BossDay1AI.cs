3c4b955b1f5421536c5af7c6784b4f974f31694b
alee12131415 <alee12131415@gmail.com>
2017-05-25 18:17:40 -0700

    final boss 1

--- Lift_V2/Assets/Scripts/ai/BossDay1AI.cs
+++ Lift_V2/Assets/Scripts/ai/BossDay1AI.cs
@@ -4,109 +4,138 @@ using UnityEngine;
 
 public class BossDay1AI : Agent {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         filename = "1.1Boss.json";
         Init();
-        isEndNode = false;
-        isExit = false;
-        Build();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        timer = currentNode.wait;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //timer -  will not start untill walkedIn is true
         if (timer > 0) timer -= Time.deltaTime;
 
-        if (!flag) flag = list[listIndex]();
+        //play normal
+        doState();
+    }
 
-        if (flag && timer <= 0) {
-            listIndex++;
-            flag = false;
-            playedFirst = false;
+    //declarations
+    private bool isPlayed = false;
+    private bool flag = false;
+    private int state = 0; //0 = outside 1 = wait to close door 2 = exposition 3 = wait to leave 4 = leave
+
+    private void doState() {
+        switch (state) {
+            case 0:
+                walkingIn();
+                break;
+            case 1:
+                closingDoor();
+                break;
+            case 2:
+                exposition();
+                break;
+            case 3:
+                readyToLeave();
+                break;
+            case 4:
+                walkingOut();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void walkingIn() {
+        if (flag) {
+            if (timer <= 0) {
+                currentNode = nodeDict["First order"];
+                isPlayed = false;
+                state = 1;
+                flag = false;
+            } return;
+        }
+        say();
+        if (enter()) {
+            if (timer > currentNode.wait) {
+                timer -= currentNode.wait;
+            } else timer = 0;
+            flag = true;
+        }
+        else if (timer <= 0) {
+            currentNode = nodeDict["Still waiting"];
             isPlayed = false;
         }
-	}
-
-    private void Build() {
-        list.Add(enterElevator);
-        list.Add(firstOrder);
-        list.Add(() => say("Day to day operations"));
-        list.Add(() => say("The Guests"));
-        list.Add(() => say("The first day"));
-        list.Add(() => say("Good start"));
-        list.Add(readyExit);
-        list.Add(playExit);
-        list.Add(() => false);
     }
 
-    private bool flag = false;
-    private bool isPlayed = false;
-    private delegate bool bossEvent();
-    private List<bossEvent> list = new List<bossEvent>();
-    private bool playedFirst = false;
-    int listIndex = 0;
-
-    private bool enterElevator() {
-        if (enter()) return true;
-
-        if (playedFirst) {
-            say("Still waiting");
-        } else {
-            say("Start");
-            playedFirst = true;
+    public void closingDoor() {
+        if (flag) {
+            if (timer <=0) {
+                currentNode = nodeDict["Day to day operations"];
+                isPlayed = false;
+                state = 2;
+                flag = false;
+            } return;
         }
-
-        if (timer <= 0) isPlayed = false;
-
-        return false;
+        say();
+        if (!isDoorOpen()) {
+            if (timer > currentNode.wait) {
+                timer -= currentNode.wait;
+            } else timer = 0;
+            flag = true;
+            return;
+        }
+        if (timer <= 0) {
+            currentNode = nodeDict["Ancient mechanism"];
+            if (isNearLever()) isPlayed = false;
+        }
     }
 
-    private bool firstOrder() {
-        if (!isDoorOpen()) return true;
+    public void exposition() {
+        say();
+        if (timer <= 0) {
+            currentNode = nodeDict[currentNode.noResponse];
+            isPlayed = false;
+        }
+        if (currentNode.name == "Extra") {
+            state = 3;
+        }
+    }
 
-        if (playedFirst) {
-            if (isNearLever() && isDoorOpen()) {
-                say("Ancient mechanism");
+    public void readyToLeave() {
+        if (timer < currentNode.wait) {
+            if (isDoorOpen()) {
+                if (getFloorNumber() == attributes.goal) state = 4;
+                else if (currentNode.name == "notFloor" && timer <= 0) {
+                    isPlayed = false;
+                    say();
+                } else if (currentNode.name != "notFloor") {
+                    currentNode = notFloorNode;
+                    isPlayed = false;
+                    say();
+                }
+            } else if (timer <= 0) {
+                currentNode = nodeDict["Extra"];
+                isPlayed = false;
+                say();
             }
-        } else {
-            say("First order");
-            playedFirst = true;
-        }
-
-        if (timer <= 0) isPlayed = false;
-
-        return false;
-    }
-
-    private bool readyExit() {
-        if (isDoorOpen()) {
-            if (getFloorNumber() != attributes.goal) say("notFloor");
-            else return true;
-        } else say("Extra");
-
-        if (timer <= 0) isPlayed = false;
-
-        return false;
-    }
-
-    private bool playExit() {
-        if (isDoorOpen() && getFloorNumber() == attributes.goal) {
-            say("End");
-            stopTalking();
-            exit();
-            return true;
-        } else {
-            listIndex -= 2;
-            return true;
         }
     }
 
-    private bool say(string name) {
-        //do not play if dialog already played -- return false
-        if (isPlayed) return false;
+    public void walkingOut() {
+        currentNode = nodeDict["End"];
+        isPlayed = false;
+        say();
+        stopTalking();
+        exit();
+        state = -1;
+    }
 
-        //get correct dialoge to play (depending on state)
-        node n = nodeDict[name];
+    private void say() {
+        //do not play if dialog already played
+        if (isPlayed) return;
 
         //get mood index
         int index = 1; //neu
@@ -114,28 +143,25 @@ public class BossDay1AI : Agent {
         else if (attributes.mood > 3) index = 0; //pos
 
         //get sound file
-        string dialogue = n.dialogue[index];
+        string dialogue = currentNode.dialogue[index];
 
         //play sound
         playDialogue(dialogue);
 
         //sets up the timer for the next node
         float audioTime = GetComponent<PatronAudio>().patronMouth.clip.length;
-        timer = audioTime + n.wait;
+        timer = audioTime + currentNode.wait;
 
         //talking animation
-        animate(n.animation[index], audioTime);
+        animate(currentNode.animation[index], audioTime);
 
         //text bubble
-        bubble.text = n.dialogue[index];
+        bubble.text = currentNode.dialogue[index];
 
         //update lastSound
         lastSound = dialogue;
 
         //update isPlayed
         isPlayed = true;
-
-        //return true when successful
-        return true;
     }
 }
