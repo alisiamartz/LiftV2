b0045188b5ce53e61492335adb2d4cd603d38a53
bion719 <djchambe@ucsc.edu>
2017-06-01 20:21:40 -0700

    Patron Dialogue Polish

--- Lift_V2/Assets/PatronAudio.cs
+++ Lift_V2/Assets/PatronAudio.cs
@@ -9,8 +9,13 @@ public class PatronAudio : MonoBehaviour {
     public string dayName;
     public string currentAudio;
 
+    private GameObject elevatorManager;
+
 	// Use this for initialization
 	void Start () {
+
+        elevatorManager = GameObject.FindGameObjectWithTag("HotelManager");
+
         if (patronMouth == null) {
             Debug.LogError(gameObject.name + " does not have a mouth assigned");
         }
@@ -18,7 +23,9 @@ public class PatronAudio : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(elevatorManager.GetComponent<FloorManager>().doorOpen == false && GetComponent<PatronMovement>().state == "leaving") {
+            patronMouth.volume = patronMouth.volume - 0.2f;
+        }
 	}
 
     public void playDialogue(string dialogue) {
